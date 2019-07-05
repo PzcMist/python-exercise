e2c53084891a359ce62403d6dfa3d275aff915eb
PzcMist <52131806+PzcMist@users.noreply.github.com>
2019-07-05 19:45:02 +0800

    Add files via upload
    Upload

---
+++ ex3.py
@@ -0,0 +1,27 @@
+print("I will now count my chickens:")
+#在屏幕上打印出I will now count my chickens:
+
+print("Hens", 25 + 30 / 6)
+#在屏幕上打印Hens，同时计算（先除在家，/表示全部除尽）打印
+print("Roosters", 100 - 25 * 3 % 4)
+#打印Roosters,同时计算（先*在 % 最后计算-）
+print("Now I will count the eggs:")
+#在屏幕上打印Now I will count the eggs:
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#先（% / 后+ -）%与/平级所以从左到右，在计算+ -从左到右
+print("Is it true that 3 + 2 < 5 - 7?")
+#在屏幕上打印Is it true that 3 + 2 < 5 - 7?
+print(3 + 2 < 5 - 7)
+#比较运算符，输出False or True
+print("What is 3 + 2?",3 + 2)
+#在屏幕上打印What is 3 + 2?，同时计算（5）
+print("What is 5 - 7?",5 - 7)
+#在屏幕上打印What is 5 - 7?，同时计算（-2）
+print("Oh,that's why it's False.")
+#在屏幕上打印出Oh,that's why it's False.
+print("How about some more.")
+#在屏幕上打印出How about some more.
+print("Is it greater?", 5> -2)
+#在屏幕上打印出Is it greater?，同时比较
+print("Is it greater or equal?",5 >= -2)
+print("Is it less or equal?", 5 <= -2)
