6d56c1d15c12f1774218870f3933254aca4497dc
PzcMist <52131806+PzcMist@users.noreply.github.com>
2019-07-06 20:26:04 +0800

    Add files via upload
    Upload

---
+++ ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, first, second, third = argv #解包
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+#从命令行输入值，然后再对应的赋值给script,first,second,third
+
+fri = input("请输入一行字符串：")
+print(fri)
